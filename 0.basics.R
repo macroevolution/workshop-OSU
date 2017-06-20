77ed3708c838da2c4caaa132ba6e58ad8275445e
Dan Rabosky <dan.rabosky@gmail.com>
2017-06-20 18:36:09 -0400

    update1

--- 0.basics.R
+++ 0.basics.R
@@ -2,7 +2,9 @@
 
 # R packages / dependencies for the course:
 
-package_string <- c("BAMMtools", "ape", "phangorn", "geiger", "diversitree", "phytools", "dev_tools", "RColorBrewer", "RPANDA", "coda", "TESS")
+package_string <- c("BAMMtools", "ape", "phangorn", "geiger", "diversitree", 
+					"phytools", "dev_tools", "RColorBrewer", 
+						"RPANDA", "coda", "TESS", "paleobioDB")
 
 install.packages(package_string)
 
